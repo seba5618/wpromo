4626ed55979a8657e1f7083a95c0e24dfcecf32d
Sebas <sebaze@gmail.comasdf>
2021-01-18 00:52:52 -0300

    SZ - leyendo prog.xml desde put request.

--- src/main/java/ar/com/bambu/wpromo/services/Promociones.java
+++ src/main/java/ar/com/bambu/wpromo/services/Promociones.java
@@ -1,8 +1,8 @@
 package ar.com.bambu.wpromo.services;
 
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.stereotype.Service;
+
 
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -12,17 +12,19 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
+
+import java.util.Date;
 
 @Service
 @Path("/promociones")
@@ -30,21 +32,36 @@ import java.nio.charset.StandardCharsets;
 public class Promociones {
 
 
-    @Produces(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_OCTET_STREAM)
     @Path("compileCod")
     @Consumes(MediaType.TEXT_XML)
     @PUT
     public Response compileCodFile(String xml) throws Exception{
-        String hola = "holassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddssssssssss";
-        ResponseEntity<ByteArrayResource> result = ResponseEntity.ok().header("Content-Disposition", "attachment; filename=promo.cod")
-                .body(new ByteArrayResource(hola.getBytes(StandardCharsets.UTF_8)));
 
-        ByteArrayOutputStream body = this.transform();
 
+        ByteArrayOutputStream body = this.transform(this.createXml(xml));
         return Response.status(HttpServletResponse.SC_CREATED).entity(body.toByteArray()).header("content-disposition", "attachment; filename=promo.pro").build();
     }
 
-    private ByteArrayOutputStream transform () throws Exception {
+    private File createXml(String xml) throws Exception {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+
+
+        Date now = new Date();
+        File file = new File(now.getTime()+"prog.xml");
+
+        file.delete();
+        file.createNewFile();
+        FileWriter writer = new FileWriter(file);
+        writer.write(xml);
+        writer.close();
+        builder.parse(file);
+        return  file;
+
+    }
+
+    private ByteArrayOutputStream transform (File xmlprog) throws Exception {
         URL resource = this.getClass().getClassLoader().getResource("plantillas/common/prog.xsl");
 
         Source xslt = new StreamSource(resource.toExternalForm());
@@ -58,7 +75,7 @@ public class Promociones {
         factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_STYLESHEET, "all");
 
         Transformer transformer = factory.newTransformer(xslt);
-        transformer.setParameter("pathProgXml", "/home/sebas/IdeaProjects/wpromo/src/main/resources/prog.xml");
+        transformer.setParameter("pathProgXml", xmlprog.getAbsolutePath());
 
         transformer.transform(xml, outt);
 
